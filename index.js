d71d1adbb5807b7dd05318d0855e799a28bc8604
Tanuj Ojha <tanujojha2042@gmail.com>
2022-01-21 01:46:26 +0530

    drumkit initial

---
+++ index.js
@@ -0,0 +1,60 @@
+// (document.querySelectorAll('.drum').length) ### this will give the number of drum buttons
+
+for (var i=0; i<document.querySelectorAll('.drum').length; i++){
+  document.querySelectorAll('.drum')[i].addEventListener("click", function(){
+    // this.style.color='black';     by this we can manipulate the css
+    var innerhtmlbutton=this.innerHTML;
+    keycheck(innerhtmlbutton);
+    animation(innerhtmlbutton);
+  });
+}
+
+document.addEventListener("keypress",function(event){
+  keycheck(event.key);
+  animation(event.key);
+});
+
+function keycheck(key){
+  switch (key) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3')
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3')
+      tom2.play();
+      break;
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case 'j':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case 'k':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    default:
+      console.log(key);
+
+  }
+}
+
+function animation(currentkey){
+  var anim = document.querySelector("." + currentkey);
+  anim.classList.add("pressed");
+
+  setTimeout(function(){
+    anim.classList.remove("pressed")
+  }, 100);
+}
